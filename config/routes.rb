f7b45f2f2b62f1253be499d927963e6b29ad6d2c
Robert Hartman <gate4hartmanr@gmail.com>
2013-01-25 17:37:49 -0800

    Add users controller, view, and test

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   root to: 'static_pages#home'
+
+  match '/signup' => 'users#new'
 
   match '/help' => 'static_pages#help'
 
   match '/about' => 'static_pages#about'
 
   match '/contact' => 'static_pages#contact'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
